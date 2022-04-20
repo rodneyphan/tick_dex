044b312f9e8b9846daa4895120ed865f3c21b801
Rodney Phan <firerachet@gmail.com>
2022-04-20 19:29:56 -0400

    Pull, Patch, And Push From Greg

--- lib/data_file.dart
+++ lib/data_file.dart
@@ -2,11 +2,14 @@
 
 import 'package:flutter/cupertino.dart';
 
+import 'p1_introduction.dart';
+import 'p4_disease_list.dart';
+
 var myNavRoutes = <String, WidgetBuilder>
 {
   'first': (context) => const Introduction(),
-  'second': (context) => const ChooseATick(),
-  'third': (context) => const Geolocation(),
+  //'second': (context) => const ChooseATick(),
+  //'third': (context) => const Geolocation(),
   'fourth': (context) => const DiseaseListDisplay()
 };
 
@@ -17,45 +20,73 @@ class Tick {
   final String scientificName;
   final String diseases;
   final String picName;
-  Tick( this.commonName, this.scientificName, this.diseases, this.picName );
-  });
+
+  Tick(this.commonName, this.scientificName, this.diseases, this.picName);
+
 }
 
-  List<Tick> tickList = [
-    Tick( "Deer or Blacklegged tick", "Ixodes scapularis", "Lyme Disease, Anaplasmosis, Babesiosis, Powassan Encephalitis", "deer_tick"),
-    Tick( "American dog tick", "Dermacentor variabilis ", "Rocky Mountain Spotted Fever, Tularemia", "american_dog_tick" ),
-    Tick( "Brown dog tick", "Rhipicephalus sanguineus", "Rocky Mountain Spotted Fever, Q Fever", "brown_dog_tick" ),
-    Tick( "Groundhog tick", "Ixodes cookei", "Powassan Encephalitis", "groundhog_tick"),
-    Tick( "Western Blacklegged tick", "Ixodes pacificus", "Borreliosis (a relapsing fever), Ehrlichiosis, Anaplasmosis", "west_blackleg_tick" ),
-    Tick( "Pacific Coast tick", "Dermacentor occidentalis", "Rocky Mountain Spotted Fever, Colorado Tick Fever, Tularemia, Q Fever", " " ),
-    Tick( "Lone Star tick", "Amblyomma americanum", "Southern Tick Rash Illness, Heartland Virus, Tularemia, Ehrlichiosis, Q Fever", "lone_star_tick" ),
-    Tick( "Rocky Mountain Wood tick", " ", "Rocky Mountain Spotted Fever, Colorado Tick Fever, Tularemia, Q Fever", "rocky_mtn_wood_tick" ),
-    Tick( "Cayenne tick", " ", "Rocky Mountain Spotted Fever", "cayenne_tick" ),
-    Tick( "Ixodes Ticks (multiple species)", "Ixodes xxxxxx", "Lyme Disease, Babesiosis, Powassan Encephalitis, Anaplasmosis", "ixodes_ticks" ),
-    Tick( "African tick", " ", "Tick-borne Relapsing Fever", "african_tick" ),
-    Tick( "Soft ticks", "Ornithodoros", "Tick-borne Relapsing Fever", "soft_ticks" )
-  ];
+List<Tick> tickList = [
+  Tick("Deer or Blacklegged tick", "Ixodes scapularis",
+      "Lyme Disease, Anaplasmosis, Babesiosis, Powassan Encephalitis",
+      "deer_tick"),
+  Tick("American dog tick", "Dermacentor variabilis ",
+      "Rocky Mountain Spotted Fever, Tularemia", "american_dog_tick"),
+  Tick("Brown dog tick", "Rhipicephalus sanguineus",
+      "Rocky Mountain Spotted Fever, Q Fever", "brown_dog_tick"),
+  Tick("Groundhog tick", "Ixodes cookei", "Powassan Encephalitis",
+      "groundhog_tick"),
+  Tick("Western Blacklegged tick", "Ixodes pacificus",
+      "Borreliosis (a relapsing fever), Ehrlichiosis, Anaplasmosis",
+      "west_blackleg_tick"),
+  Tick("Pacific Coast tick", "Dermacentor occidentalis",
+      "Rocky Mountain Spotted Fever, Colorado Tick Fever, Tularemia, Q Fever",
+      " "),
+  Tick("Lone Star tick", "Amblyomma americanum",
+      "Southern Tick Rash Illness, Heartland Virus, Tularemia, Ehrlichiosis, Q Fever",
+      "lone_star_tick"),
+  Tick("Rocky Mountain Wood tick", " ",
+      "Rocky Mountain Spotted Fever, Colorado Tick Fever, Tularemia, Q Fever",
+      "rocky_mtn_wood_tick"),
+  Tick("Cayenne tick", " ", "Rocky Mountain Spotted Fever", "cayenne_tick"),
+  Tick("Ixodes Ticks (multiple species)", "Ixodes xxxxxx",
+      "Lyme Disease, Babesiosis, Powassan Encephalitis, Anaplasmosis",
+      "ixodes_ticks"),
+  Tick("African tick", " ", "Tick-borne Relapsing Fever", "african_tick"),
+  Tick("Soft ticks", "Ornithodoros", "Tick-borne Relapsing Fever", "soft_ticks")
+];
 
 class Disease {
   String disName;
   String disCasesPerYear;
   String disURL;
-  Disease( this.disName, this.disCasesPerYear, this.disURL );
+
+  Disease(this.disName, this.disCasesPerYear, this.disURL);
 }
 
 List<Disease> diseasesList = [
-    Disease( "Lyme Disease", "34945", "https://www.cdc.gov/niosh/topics/tick-borne/default.html" ),
-    Disease( "Anaplasmosis", "5655", "https://www.health.state.mn.us/diseases/anaplasmosis/basics.html" ),
-    Disease( "Rocky Mountain Spotted Fever", "5207", "https://www.cdc.gov/rmsf/stats/index.html" ),
-    Disease( "Babesiosis", "2420", "https://www.cdc.gov/parasites/babesiosis/index.html" ),
-    Disease( "Ehrlichiosis", "2093", "https://www.cdc.gov/ehrlichiosis/symptoms/index.html" ),
-    Disease( "Tularemia", "274", "https://www.mayoclinic.org/diseases-conditions/tularemia/symptoms-causes/syc-20378635" ),
-    Disease( "Q Fever", "179", "https://www.cdc.gov/qfever/index.html, https://www.mayoclinic.org/diseases-conditions/q-fever/symptoms-causes/syc-20352995"),
-    Disease( "Powassan Encephalitis", "43", "https://www.cdc.gov/powassan/index.html" ),  
-    Disease( "Tick-borne Relapsing Fever", "23", "https://doh.wa.gov/you-and-your-family/illness-and-disease-z/tick-borne-relapsing-fever" ),
-    Disease( "Colorado Tick Fever", "6", "https://www.cdc.gov/coloradotickfever/index.html" ),
-    Disease( "Southern Tick Rash Illness", "Not Reported", "https://www.floridahealth.gov/diseases-and-conditions/southern-tick-rash-illness/index.html")
-  ];
+  Disease("Lyme Disease", "34945",
+      "https://www.cdc.gov/niosh/topics/tick-borne/default.html"),
+  Disease("Anaplasmosis", "5655",
+      "https://www.health.state.mn.us/diseases/anaplasmosis/basics.html"),
+  Disease("Rocky Mountain Spotted Fever", "5207",
+      "https://www.cdc.gov/rmsf/stats/index.html"),
+  Disease("Babesiosis", "2420",
+      "https://www.cdc.gov/parasites/babesiosis/index.html"),
+  Disease("Ehrlichiosis", "2093",
+      "https://www.cdc.gov/ehrlichiosis/symptoms/index.html"),
+  Disease("Tularemia", "274",
+      "https://www.mayoclinic.org/diseases-conditions/tularemia/symptoms-causes/syc-20378635"),
+  Disease("Q Fever", "179",
+      "https://www.cdc.gov/qfever/index.html, https://www.mayoclinic.org/diseases-conditions/q-fever/symptoms-causes/syc-20352995"),
+  Disease(
+      "Powassan Encephalitis", "43", "https://www.cdc.gov/powassan/index.html"),
+  Disease("Tick-borne Relapsing Fever", "23",
+      "https://doh.wa.gov/you-and-your-family/illness-and-disease-z/tick-borne-relapsing-fever"),
+  Disease("Colorado Tick Fever", "6",
+      "https://www.cdc.gov/coloradotickfever/index.html"),
+  Disease("Southern Tick Rash Illness", "Not Reported",
+      "https://www.floridahealth.gov/diseases-and-conditions/southern-tick-rash-illness/index.html")
+];
 
 
 // ------------- End of data_file.dart ---------------
--- lib/p4_disease_list.dart
+++ lib/p4_disease_list.dart
@@ -3,38 +3,33 @@ import 'data_file.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DiseaseListDisplay extends StatefulWidget {
-
-  const DiseaseListDisplay ({Key? key}) : super(key: key);
+  const DiseaseListDisplay({Key? key}) : super(key: key);
 
   @override
   DiseaseListState createState() => DiseaseListState();
 }
 
 class DiseaseListState extends State<DiseaseListDisplay> {
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
-  void initState() { super.initState(); }
-
-  @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Tick-Borne Illnesses"),
         ),
         body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DisplayDiseaseList( )
-        )
-    );
+            padding: const EdgeInsets.all(8.0), child: DisplayDiseaseList()));
   }
-
 } // end of DiseaseListState
 
 class DisplayDiseaseList extends StatelessWidget {
+  DisplayDiseaseList({Key? key}) : super(key: key);
 
-  DisplayDiseaseList ({Key? key}) : super(key: key);
-
-  final List<Disease> listOfDiseases = diseaseList;
+  final List<Disease> listOfDiseases = diseasesList;
 
   @override
   Widget build(BuildContext context) {
@@ -46,27 +41,29 @@ class DisplayDiseaseList extends StatelessWidget {
 
   Widget buildListBody(BuildContext context, int index) {
     return Row(
-      children: <Widget> [
-        SizedBox( width: 600,
+      children: <Widget>[
+        SizedBox(
+          width: 600,
           child: ListTile(
-              title: Text(listOfDiseases[ index ].disName, textScaleFactor: 1.5 ),
-              subtitle: Text( "  Cases in US for 2019:  " + listOfDiseases[index].disCasesPerYear),
-            ),
+            title: Text(listOfDiseases[index].disName, textScaleFactor: 1.5),
+            subtitle: Text("  Cases in US for 2019:  " +
+                listOfDiseases[index].disCasesPerYear),
+          ),
         ),
         ElevatedButton(
           child: const Text("Go to website for more information"),
-          onPressed: () => onBtnClicked( listOfDiseases[index].disURL),
+          onPressed: () => onBtnClicked(listOfDiseases[index].disURL),
         ),
       ],
     );
   }
 
-  onBtnClicked (String url) async {
-
-    if ( await canLaunch(url) )
-    { await launch(url); }
-    else { throw 'Could not launch $url'; }
+  onBtnClicked(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
-
 }
 // end of p4_disease_list.dart
