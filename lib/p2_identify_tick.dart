b3d8129138bc5e140594144d4d03fea540a76ac2
gabaugher <103779452+gabaugher@users.noreply.github.com>
2022-04-26 21:59:44 -0400

    Update p2_identify_tick.dart
    Just need to do the enlarging to fullscreen thing.

--- lib/p2_identify_tick.dart
+++ lib/p2_identify_tick.dart
@@ -1,6 +1,5 @@
 //  p2_identify_tick.dart
 
-
 import 'package:flutter/material.dart';
 import 'data_file.dart';
 
@@ -21,13 +20,12 @@ class TickListState extends State<TickListDisplay> {
   Widget build (BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Tick-Identifier\nClick on the identified tick's row:"),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: DisplayTickList( )
-        )
-    );
+          title: const Text("Identifier - Click on tick name. (Make note of sex/stage.)\nClick the picture to enlarge."),
+          ),
+        body: Padding( padding: const EdgeInsets.all(8.0),
+              child: DisplayTickList( )
+          ),
+      );
   }
 
 } // end of TickListState
@@ -39,7 +37,7 @@ class DisplayTickList extends StatelessWidget {
   DisplayTickList({Key? key}) : super(key: key);
 
   final List<Tick> listOfTicks = tickList;
-  int tappedIndex = 0;
+  int tappedIndex = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -50,38 +48,56 @@ class DisplayTickList extends StatelessWidget {
   }
 
   Widget buildListBody(BuildContext context, int index) {
-    String imageLocation = 'assets/images/' + listOfTicks[index].picName + '.png';
+
+    String imageLocation = 'images/' + listOfTicks[index].picName + '.png';
 
     return
-      SizedBox(width: 500, height: 300 ,
-        child: ListTile(
+      ListTile(
           leading: GestureDetector(behavior: HitTestBehavior.translucent,
-            onTap: () { tappedIndex = index; RadioList(); },
-            child: Container(width: 150, height: 300,
+            onTap: () { tappedIndex = index;  },
+          child: Container(width: 90, height: 160,
               color: tappedIndex == index? Colors.blue : Colors.grey,
               padding: const EdgeInsets.all(3.0),
               alignment: Alignment.centerLeft,
-              child: Image.asset(imageLocation, height: 300, scale: 2.0, ),
+              child: SizedBox( width: 300, height: 300,
+                child: Expanded( child: Image.asset(imageLocation, height: 300, ), ),
+              ),
             ),
           ),
           title: Text(listOfTicks[ index ].commonName + " (" +
-              listOfTicks[index].scientificName + ")", textScaleFactor: 1.5),
+              listOfTicks[index].scientificName + ")", textScaleFactor: 1.2),
           subtitle: Text(
               "Associated illnesses: " + listOfTicks[index].diseases),
-        ),
+          onTap: ( ) => _onContactTapped( context, listOfTicks[index] ),
       );
   }
 }
 
-class RadioList extends StatefulWidget {
-  const RadioList({Key? key}) : super(key: key);
+void _onContactTapped( BuildContext context, Tick tappedContact) {
+  ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar (
+        content: Column(
+          children: <Widget> [
+            Text( "Name of tick: " + tappedContact.commonName + "\n\nNow click: Male, Female, Larva, or Nymph\n", textScaleFactor: 1.5,),
+            const RadioList(),
+            ],
+          ),
+        duration: const Duration( seconds: 8, ),
+        backgroundColor: Colors.lightGreen,
+        // action:
+      )
+      ); // showSnackBar
+  }
+
+  class RadioList extends StatefulWidget {
+    const RadioList({Key? key}) : super(key: key);
 
   @override
   State<RadioList> createState() => _RadioListState();
-}
+  }
 
-class _RadioListState extends State<RadioList> {
-  Choice? _choice = Choice.male;
+  class _RadioListState extends State<RadioList> {
+    Choice? _choice = Choice.male;
 
   @override
   Widget build(BuildContext context) {
@@ -89,34 +105,37 @@ class _RadioListState extends State<RadioList> {
       children: <Widget>[
         ListTile( title: const Text('Male'),
           leading: Radio<Choice> (
-            value: Choice.male,
-            groupValue: _choice,
-            onChanged: (Choice? value) { setState(() { _choice = value; }); },
+          value: Choice.male,
+          groupValue: _choice,
+          onChanged: (Choice? value) { setState(() { _choice = value; }); },
+            ),
           ),
-        ),
         ListTile( title: const Text('Female'),
           leading: Radio<Choice> (
-            value: Choice.female,
-            groupValue: _choice,
-            onChanged: (Choice? value) { setState(() { _choice = value; }); },
+          value: Choice.female,
+          groupValue: _choice,
+          onChanged: (Choice? value) { setState(() { _choice = value; }); },
+            ),
           ),
-        ),
         ListTile( title: const Text('Larva'),
           leading: Radio<Choice> (
-            value: Choice.larva,
-            groupValue: _choice,
-            onChanged: (Choice? value) { setState(() { _choice = value; }); },
+          value: Choice.larva,
+          groupValue: _choice,
+          onChanged: (Choice? value) { setState(() { _choice = value; }); },
+            ),
           ),
-        ),
         ListTile( title: const Text('Nymph'),
           leading: Radio<Choice> (
-            value: Choice.nymph,
-            groupValue: _choice,
-            onChanged: (Choice? value) { setState(() { _choice = value; }); },
+          value: Choice.nymph,
+          groupValue: _choice,
+          onChanged: (Choice? value) { setState(() { _choice = value; }); },
+            ),
           ),
-        ),
-      ],
-    );
+        ],
+      );
   }
 
 }
+
+
+
