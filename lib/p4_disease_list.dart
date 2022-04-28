d17030ebc99f5127d0055af7586dbb7611754861
gabaugher <103779452+gabaugher@users.noreply.github.com>
2022-04-28 12:23:21 -0400

    Update p4_disease_list.dart

--- lib/p4_disease_list.dart
+++ lib/p4_disease_list.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'data_file.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class DiseaseListDisplay extends StatefulWidget {
-  const DiseaseListDisplay({Key? key}) : super(key: key);
+
+  const DiseaseListDisplay ({Key? key}) : super(key: key);
 
   @override
   DiseaseListState createState() => DiseaseListState();
 }
 
 class DiseaseListState extends State<DiseaseListDisplay> {
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() { super.initState(); }
+
+  @override
+  Widget build (BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Tick-Borne Illnesses"),
+        appBar: AppBar( backgroundColor: Colors.yellowAccent,
+          title: const Text("Tick-Borne Illnesses in the US", style: TextStyle( color: Colors.brown) ),
         ),
         body: Padding(
-            padding: const EdgeInsets.all(8.0), child: DisplayDiseaseList()));
+            padding: const EdgeInsets.all(8.0),
+            child: DisplayDiseaseList( )
+        )
+    );
   }
+
 } // end of DiseaseListState
 
 class DisplayDiseaseList extends StatelessWidget {
-  DisplayDiseaseList({Key? key}) : super(key: key);
+
+  DisplayDiseaseList ({Key? key}) : super(key: key);
 
   final List<Disease> listOfDiseases = diseaseList;
 
@@ -41,29 +46,28 @@ class DisplayDiseaseList extends StatelessWidget {
 
   Widget buildListBody(BuildContext context, int index) {
     return Row(
-      children: <Widget>[
-        SizedBox(
-          width: 300,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget> [
+        SizedBox( width: 380,
           child: ListTile(
-            title: Text(listOfDiseases[index].disName, textScaleFactor: 1.2),
-            subtitle: Text("  Cases in US for 2019:  " +
-                listOfDiseases[index].disCasesPerYear),
-          ),
+              title: Text(listOfDiseases[ index ].disName, textScaleFactor: 1.3, style: const TextStyle( color: Colors.brown) ),
+              subtitle: Text( "  Cases in US for 2019:  " + listOfDiseases[index].disCasesPerYear, style: const TextStyle( color: Colors.green ) ),
+            ),
         ),
         ElevatedButton(
-          child: const Text("More Information"),
-          onPressed: () => onBtnClicked(listOfDiseases[index].disURL),
+          child: const Text("Go to a website"),
+          onPressed: () => onBtnClicked( listOfDiseases[index].disURL),
         ),
       ],
     );
   }
 
-  onBtnClicked(String url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+  onBtnClicked (String url) async {
+
+    if ( await canLaunchUrlString(url) )
+    { await launchUrlString(url); }
+    else { throw 'Could not launch $url'; }
   }
+
 }
 // end of p4_disease_list.dart
